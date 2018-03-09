c430a3492c4c1434061cb7dd20f9ace2672c6b50
Gregor Mazovec <gregor.mazovec@gmail.com>
2018-03-09 21:31:20 +0100

    Add tests for unionOf and tupleOf cardinality

--- test/tupleof.js
+++ test/tupleof.js
@@ -2,9 +2,63 @@
 import test from 'ava-spec'
 import typer from '../src'
 
-const { tupleOf1, tupleOf3, boolean, number, string, objectOf, arrayOf } = typer
+const {
+  tupleOf1,
+  tupleOf2,
+  tupleOf3,
+  tupleOf4,
+  tupleOf5,
+  boolean,
+  number,
+  string,
+  object,
+  objectOf,
+  arrayOf
+} = typer
 
-test.group('primitive types', test => {
+test.group('primitive types - cardinality 1', test => {
+  const validator = tupleOf1(boolean)
+
+  test('should validate a tuple', t => {
+    const input = [true]
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError)
+    t.throws(() => { validator(undefined) }, TypeError)
+    t.throws(() => { validator(true) }, TypeError)
+    t.throws(() => { validator(12345) }, TypeError)
+    t.throws(() => { validator('foo') }, TypeError)
+    t.throws(() => { validator([]) }, TypeError)
+    t.throws(() => { validator([true, false]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 2', test => {
+  const validator = tupleOf2(number, number)
+
+  test('should validate a tuple', t => {
+    const input = [34, 65]
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError)
+    t.throws(() => { validator(undefined) }, TypeError)
+    t.throws(() => { validator(true) }, TypeError)
+    t.throws(() => { validator(12345) }, TypeError)
+    t.throws(() => { validator('foo') }, TypeError)
+    t.throws(() => { validator({}) }, TypeError)
+    t.throws(() => { validator([]) }, TypeError)
+    t.throws(() => { validator(['234', '64']) }, TypeError)
+    t.throws(() => { validator([123, 54, 256]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 3', test => {
   const validator = tupleOf3(string, number, boolean)
 
   test('should validate a tuple', t => {
@@ -22,7 +76,52 @@ test.group('primitive types', test => {
     t.throws(() => { validator({}) }, TypeError)
     t.throws(() => { validator([]) }, TypeError)
     t.throws(() => { validator(['foo', '12345', 'true']) }, TypeError)
+    t.throws(() => { validator(['foo', '12345', 'true', 0]) }, TypeError)
     t.throws(() => { validator(['foo', 12345]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 4', test => {
+  const validator = tupleOf4(number, number, boolean, boolean)
+
+  test('should validate a tuple', t => {
+    const input = [34, 65, true, false]
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError)
+    t.throws(() => { validator(undefined) }, TypeError)
+    t.throws(() => { validator(true) }, TypeError)
+    t.throws(() => { validator(12345) }, TypeError)
+    t.throws(() => { validator('foo') }, TypeError)
+    t.throws(() => { validator({}) }, TypeError)
+    t.throws(() => { validator([]) }, TypeError)
+    t.throws(() => { validator(['234', '64', true, false]) }, TypeError)
+    t.throws(() => { validator([46, 34, true, true, true]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 5', test => {
+  const validator = tupleOf5(string, string, string, string, string)
+
+  test('should validate a tuple', t => {
+    const input = ['H', 'e', 'l', 'l', 'o']
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError)
+    t.throws(() => { validator(undefined) }, TypeError)
+    t.throws(() => { validator(true) }, TypeError)
+    t.throws(() => { validator(12345) }, TypeError)
+    t.throws(() => { validator('foo') }, TypeError)
+    t.throws(() => { validator({}) }, TypeError)
+    t.throws(() => { validator([]) }, TypeError)
+    t.throws(() => { validator(['o', 'l', 'l', 'e', 'H', '.']) }, TypeError)
+    t.throws(() => { validator(['foo', 'bar', 0, 'kit', 'kat']) }, TypeError)
   })
 })
 
--- test/unionof.js
+++ test/unionof.js
@@ -2,9 +2,23 @@
 import test from 'ava-spec'
 import typer from '../src'
 
-const { unionOf2, literalOf } = typer
+const {
+  unionOf2,
+  unionOf3,
+  unionOf4,
+  unionOf5,
+  arrayOf,
+  objectOf,
+  string,
+  number,
+  boolean,
+  nil,
+  undef,
+  tupleOf3,
+  literalOf
+} = typer
 
-test.group('primitive types', test => {
+test.group('primitive types - cardinality 2', test => {
   const validator = unionOf2(
     literalOf(('foo' /*: 'foo' */)),
     literalOf((12345 /*: 12345 */))
@@ -13,5 +27,111 @@ test.group('primitive types', test => {
   test('should validate an union', t => {
     t.is(validator('foo'), 'foo')
     t.is(validator(12345), 12345)
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError),
+    t.throws(() => { validator(undefined) }, TypeError),
+    t.throws(() => { validator(true) }, TypeError),
+    t.throws(() => { validator(1234) }, TypeError),
+    t.throws(() => { validator('bar') }, TypeError),
+    t.throws(() => { validator({}) }, TypeError),
+    t.throws(() => { validator([]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 4', test => {
+  const validator = unionOf4(
+    literalOf((false /*: false */)),
+    literalOf((0 /*: 0 */)),
+    literalOf((null /*: null */)),
+    literalOf(('' /*: '' */)),
+  )
+
+  test('should validate an union', t => {
+    t.is(validator(false), false)
+    t.is(validator(0), 0)
+    t.is(validator(null), null)
+    t.is(validator(''), '')
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator(undefined) }, TypeError),
+    t.throws(() => { validator(true) }, TypeError),
+    t.throws(() => { validator(1234) }, TypeError),
+    t.throws(() => { validator('bar') }, TypeError),
+    t.throws(() => { validator({}) }, TypeError),
+    t.throws(() => { validator([]) }, TypeError)
+  })
+})
+
+test.group('primitive types - cardinality 5', test => {
+  const validator = unionOf5(
+    nil,
+    undef,
+    boolean,
+    number,
+    string
+  )
+
+  test('should validate an union', t => {
+    t.is(validator(false), false)
+    t.is(validator(true), true)
+    t.is(validator(0), 0)
+    t.is(validator(9), 9)
+    t.is(validator(null), null)
+    t.is(validator(undefined), undefined)
+    t.is(validator(''), '')
+    t.is(validator('foo'), 'foo')
+  })
+
+  test('should throw an error', t => {
+    t.throws(() => { validator({}) }, TypeError),
+    t.throws(() => { validator([]) }, TypeError)
+  })
+})
+
+test.group('composable types', test => {
+  const validator = unionOf3(
+    objectOf(o => ({
+      type: unionOf2(
+        literalOf(('text' /*: 'text' */)),
+        literalOf(('image' /*: 'image' */))
+      )(o.type),
+      content: string(o.content),
+      enabled: boolean(o.enabled)
+    })),
+    arrayOf(string),
+    tupleOf3(string, string, number)
+  )
+
+  test('should validate an union #1', t => {
+    const input = { type: 'text', content: 'Hello', enabled: true }
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should validate an union #2', t => {
+    const input = ['this', 'is', 'an', 'array', 'of', 'strings']
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('should validate an union #3', t => {
+    const input = ['Bob', 'bob@example.net', 43]
+    const value = validator(input)
+    t.deepEqual(value, input)
+  })
+
+  test('shold throw an error', t => {
+    t.throws(() => { validator(null) }, TypeError)
+    t.throws(() => { validator(undefined) }, TypeError)
+    t.throws(() => { validator(true) }, TypeError)
+    t.throws(() => { validator(12345) }, TypeError)
+    t.throws(() => { validator('foo') }, TypeError)
+    t.throws(() => { validator({ type: 'widge', content: 'Hello', enabled: true }) }, TypeError)
+    t.throws(() => { validator({ type: 'widge', content: 'Hello', enabled: 1 }) }, TypeError)
+    t.throws(() => { validator(['this', 'is', 1, 'array', 'of', 'strings']) }, TypeError)
+    t.throws(() => { validator(['bob', 'bob@example.net', 36, true]) }, TypeError)
   })
 })
