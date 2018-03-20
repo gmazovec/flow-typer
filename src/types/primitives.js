3dc1813b905d43c629077d4ddc36350e9372d765
Gregor Mazovec <gregor.mazovec@gmail.com>
2018-03-20 23:44:40 +0100

    Change code style for flow annotations

--- src/types/primitives.js
+++ src/types/primitives.js
@@ -11,35 +11,47 @@ const {
   isObject
 } = require('../is')
 
-exports.nil =
-  function nil (v: mixed): null {
+import type { TypeValidator } from '..'
+
+exports.nil = (
+  function nil (v) {
     if (isEmpty(v) || isNil(v)) return null
     throw error('null', typeof v)
   }
+  : TypeValidator<null>
+)
 
-exports.undef =
-  function undef (v: mixed): void {
+exports.undef = (
+  function undef (v) {
     if (isEmpty(v) || isUndef(v)) return undefined
     throw error('undefined', typeof v)
   }
+  : TypeValidator<void>
+)
 
-exports.boolean =
-  function boolean (v: mixed): boolean {
+exports.boolean = (
+  function boolean (v) {
     if (isEmpty(v)) return false
     if (isBoolean(v)) return v
     throw error('boolean', typeof v)
   }
+  : TypeValidator<boolean>
+)
 
-exports.number =
-  function number (v: mixed): number {
+exports.number = (
+  function number (v) {
     if (isEmpty(v)) return 0
     if (isNumber(v)) return v
     throw error('number', typeof v)
   }
+  : TypeValidator<number>
+)
 
-exports.string =
-  function string (v: mixed): string {
+exports.string = (
+  function string (v) {
     if (isEmpty(v)) return ''
     if (isString(v)) return v
     throw error('string', typeof v)
   }
+  : TypeValidator<string>
+)
