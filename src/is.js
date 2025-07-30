b2473013c369922cd451a5ea4daa82eb918cae9d
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-07-30 16:42:53 +0200

    Predicate to guards

--- src/is.js
+++ src/is.js
@@ -1,27 +1,27 @@
 // @flow
 
 const isNil =
-  (v: mixed): boolean %checks =>
+  (v: mixed) /*: v is null */ =>
     v === null
 
 exports.isNil = isNil
 
 exports.isUndef =
-  (v: mixed): boolean %checks =>
+  (v: mixed) /*: v is void */ =>
     typeof v === 'undefined'
 
 exports.isBoolean =
-  (v: mixed): boolean %checks =>
+  (v: mixed) /*: v is boolean */ =>
     typeof v === 'boolean'
 
 exports.isNumber =
-  (v: mixed): boolean %checks =>
+  (v: mixed) /*: v is number */ =>
     typeof v === 'number'
 
 exports.isString =
-  (v: mixed): boolean %checks =>
+  (v: mixed) /*: v is string */ =>
     typeof v === 'string'
 
 exports.isObject =
-  (v: mixed): boolean %checks =>
-    !isNil(v) && typeof v === 'object'
+  (v: mixed) /*: v is {...} */ =>
+    v !== null && typeof v === 'object'
