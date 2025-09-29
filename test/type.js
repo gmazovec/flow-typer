2362ab2e0dd4a8b8ff76b2c1604e659ff22d7da8
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-09-29 14:11:02 +0200

    Update import paths

--- test/type.js
+++ test/type.js
@@ -1,8 +1,8 @@
 // @flow
 
-const { objectOf, string, number, maybe, boolean, unionOf, arrayOf, tupleOf, literalOf } = require('./src/index.js');
+const { objectOf, string, number, maybe, boolean, unionOf, arrayOf, tupleOf, literalOf } = require('../src/index.js');
 
-/*:: import type { $Literal } from './src/index.js'; */
+/*:: import type { $Literal } from '../src/index.js'; */
 
 const male = (literalOf('male')/*: $Literal<"male"> */);
 const female = (literalOf('female')/*: $Literal<"female"> */);
