d7078370a2017046fbfafa87955a6bbbec0e6f53
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-10-24 17:07:09 +0200

    Use paramater as variable

--- src/types/array.js
+++ src/types/array.js
@@ -7,11 +7,11 @@ import type { TypeValidator, TypeArrayValidator } from '..'
 
 export const arrayOf =
   <T>(typeFn: TypeValidator<T>, label?: string = 'Array'): TypeArrayValidator<T> => {
-    function array (value: mixed, _scope: string = label): T[] {
+    function array (value: mixed, _scope: string = label, _index: number = 0): T[] {
       if (value === EMPTY_VALUE) return [typeFn(value)]
       if (Array.isArray(value)) {
-        let index = 0
-        return value.map(v => typeFn(v, `${_scope}[${index++}]`))
+        _index = 0;
+        return value.map(v => typeFn(v, `${_scope}[${_index++}]`))
       }
       throw validatorError(array, value, _scope)
     }
