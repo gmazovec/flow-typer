6a6c95f80c7cd696c36bc66e7b6bde91969a4d5f
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-11-20 14:35:07 +0100

    Add union/tupleOf type tests

--- test/type.js
+++ test/type.js
@@ -3,8 +3,8 @@
 const t = require('../src/index.js');
 
 const { objectOf, string, number, maybe, boolean, arrayOf, literalOf, type } = t;
-const { tupleOf, tupleOf2, tupleOf3 } = t;
-const { unionOf, unionOf2 } = t;
+const { tupleOf, tupleOf2, tupleOf3, tupleOf4, tupleOf5, tupleOf6 } = t;
+const { unionOf, unionOf2, unionOf3, unionOf4, unionOf5, unionOf6 } = t;
 
 /*:: import type { $Literal } from '../src/index.js'; */
 
@@ -64,34 +64,31 @@ const v2 = timeType(eval('new Time();'));
 */
 
 const u2 = unionOf2(string, number)('');
+const u3 = unionOf3(string, number, boolean)('');
+const u4 = unionOf4(string, number, boolean, arrayOf(string))('');
+const u5 = unionOf5(string, number, boolean, arrayOf(string), arrayOf(number))('');
+const u6 = unionOf6(string, number, boolean, arrayOf(string), arrayOf(number), arrayOf(boolean))('');
 
-/*::
-
-(u2: string | number);
-
-*/
+/*:: (u2: string | number); */
+/*:: (u3: string | number | boolean); */
+/*:: (u4: string | number | boolean | string[]); */
+/*:: (u5: string | number | boolean | string[] | number[]); */
+/*:: (u6: string | number | boolean | string[] | number[] | boolean[]); */
 
 const u2t = unionOf2(tupleOf(string, string), tupleOf(number, number))([0, 0]);
 
-/*::
-
-(u2t : [string, string] | [number, number]);
-
-*/
+/*:: (u2t : [string, string] | [number, number]); */
 
 const t2 = tupleOf2(number, string)(['1', '1']);
-
-/*::
-
-(t2 : [number, string]);
-
-*/
-
 const t3 = tupleOf3(number, number, number)(['0', '1', '1']);
+const t4 = tupleOf4(number, string, string, string)(['0', '0', '1']);
+const t5 = tupleOf5(string, number, number, number, number)(['0', '1', '1', '0', '0']);
+const t6 = tupleOf6(string, number, number, number, number, boolean)(['1', '1', '1', '0', '0', '0']);
 
-/*::
+/*:: (t2 : [number, string]); */
+/*:: (t3: [number, number, number]); */
+/*:: (t4: [number, string, string, string]); */
+/*:: (t5: [string, number, number, number, number]); */
+/*:: (t6: [string, number, number, number, number, boolean]); */
 
-(t3: [number, number, number]);
-
-*/
 
