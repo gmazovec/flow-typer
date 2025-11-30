ba44995e9424e2d1b7069c3bc9a937b68509d775
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-11-30 21:52:31 +0100

    Deprecate literalOf

--- src/types/literals.js
+++ src/types/literals.js
@@ -2,11 +2,13 @@
 import { validatorError } from '../error.js'
 import { isBoolean } from '../is.js'
 import { EMPTY_VALUE } from '../const.js'
+import { deprwarn } from '../index.js'
 
 import type { LiteralValue, TypeValidator } from '..'
 
 export const literalOf =
   <T: LiteralValue>(primitive: T): TypeValidator<T> => {
+    deprwarn('calling literalOf is deprecated; use validator literal instead', 'DEP001')
     function literal (value: mixed, _scope: string = ''): T {
       if (value === EMPTY_VALUE || (value === primitive)) return primitive
       throw validatorError(literal, value, _scope)
