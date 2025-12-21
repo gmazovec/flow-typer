466e34e758871c232f79536cd45cfe95a441246b
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-12-21 14:17:23 +0100

    separate casting from validation

--- src/types/array.js
+++ src/types/array.js
@@ -4,11 +4,20 @@ import { validatorError } from '../error.js'
 
 import type { TypeValidator, TypeArrayValidator, TypeAssertError } from '..'
 
+const toArray = (
+  function <T>(typeFn: TypeValidator<T>, value: mixed, _scope: string): Array<T> | void {
+    if (Array.isArray(value)) {
+      return value.map((v, i) => typeFn(v, `${_scope}[${i}]`))
+    }
+  }
+)
+
 export const arrayOf =
   <T>(typeFn: TypeValidator<T>, label?: string = 'Array'): TypeArrayValidator<T> => {
     function array (value: mixed, _scope: string = label, err: ?TypeAssertError[]): Array<T> {
-      if (Array.isArray(value)) {
-        return value.map((v, i) => typeFn(v, `${_scope}[${i}]`))
+      const v = toArray(typeFn, value, _scope)
+      if (v !== undefined) {
+        return v;
       }
       if (err) {
         err.push({ expected: array.type(), actual: typeof(value), scope: _scope })
