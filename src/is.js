f6ed632e79b66cb7ef74a5b7959846c4877c02d1
Gregor Mazovec <gregor.mazovec@gmail.com>
2025-12-24 14:05:30 +0100

    allow mixing type guards

--- src/is.js
+++ src/is.js
@@ -3,31 +3,31 @@
 import { deprwarn } from './index.js'
 
 export const isNil =
-  (v: mixed) /*: v is null */ => {
+  (v: mixed) /*: implies v is null */ => {
     deprwarn('use of isNil validator is deprecated; use isNull instead', 'FT004')
     return v === null
   }
 
 export const isNull =
-  (v: mixed) /*: v is null */ =>
+  (v: mixed) /*: implies v is null */ =>
     v === null
 
 export const isUndef =
-  (v: mixed) /*: v is void */ =>
+  (v: mixed) /*: implies v is void */ =>
     typeof v === 'undefined'
 
 export const isBoolean =
-  (v: mixed) /*: v is boolean */ =>
+  (v: mixed) /*: implies v is boolean */ =>
     typeof v === 'boolean'
 
 export const isNumber =
-  (v: mixed) /*: v is number */ =>
+  (v: mixed) /*: implies v is number */ =>
     typeof v === 'number'
 
 export const isString =
-  (v: mixed) /*: v is string */ =>
+  (v: mixed) /*: implies v is string */ =>
     typeof v === 'string'
 
 export const isObject =
-  (v: mixed) /*: v is {...} */ =>
+  (v: mixed) /*: implies v is {...} */ =>
     v !== null && typeof v === 'object'
