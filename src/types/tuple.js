f98cb9e686424a4345b3c1828eeccf27579d3e4c
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-02-03 14:18:38 +0100

    assert context for tuple validators

--- src/types/tuple.js
+++ src/types/tuple.js
@@ -1,9 +1,10 @@
 // @flow
+import { assertContext } from '../type.js'
 import { getType } from '../utils.js'
 import { validatorError, validatorTypeError } from '../error.js'
 import { deprwarn } from '../index.js'
 
-import type { TypeValidator } from '../'
+import type { TypeValidator, TypeAssertError, AssertionContext } from '../'
 
 export const tupleOf: Tuple2TypeValidator = function tupleOf_ (va, vb) {
   deprwarn('calling tupleOf is deprecated; fallback to tupleOf2; use validators with arity, ex. tupleOf2, ... tupleOf6', 'FT002')
@@ -13,83 +14,107 @@ export const tupleOf: Tuple2TypeValidator = function tupleOf_ (va, vb) {
 type Tuple1TypeValidator = <A> (TypeValidator<A>) => TypeValidator<[A]>
 
 export const tuple1: Tuple1TypeValidator = function (va) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}]`
+  const tuple_value = () => [va.value()]
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 1) {
       return [va(value[0], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}]`
-  tuple.value = () => [va.value()]
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
   return tuple
 }
 
 type Tuple2TypeValidator = <A, B> (TypeValidator<A>, TypeValidator<B>) => TypeValidator<[A, B]>
 
 export const tuple2: Tuple2TypeValidator = function (va, vb) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}, ${getType(vb)}]`
+  const tuple_value = () => [va.value(), vb.value()];
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 2) {
       return [va(value[0], _scope), vb(value[1], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}, ${getType(vb)}]`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}, ${getType(vb)}]`
-  tuple.value = () => [va.value(), vb.value()];
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
   return tuple
 }
 
 type Tuple3TypeValidator = <A, B, C> (TypeValidator<A>, TypeValidator<B>, TypeValidator<C>) => TypeValidator<[A, B, C]>
 
 export const tuple3: Tuple3TypeValidator = function (va, vb, vc) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}]`
+  const tuple_value = () => [va.value(), vb.value(), vc.value()];
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 3) {
       return [va(value[0], _scope), vb(value[1], _scope), vc(value[2], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}, ${getType(vb)}, ${getType(vc)}]`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}]`
-  tuple.value = () => [va.value(), vb.value(), vc.value()];
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
   return tuple
 }
 
 type Tuple4TypeValidator = <A, B, C, D> (TypeValidator<A>, TypeValidator<B>, TypeValidator<C>, TypeValidator<D>) => TypeValidator<[A, B, C, D]>
 
 export const tuple4: Tuple4TypeValidator = function (va, vb, vc, vd) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}]`
+  const tuple_value = () => [va.value(), vb.value(), vc.value(), vd.value()];
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 4) {
       return [va(value[0], _scope), vb(value[1], _scope), vc(value[2], _scope), vd(value[3], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}]`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}]`
-  tuple.value = () => [va.value(), vb.value(), vc.value(), vd.value()];
-  return tuple
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
+  return tuple;
 }
 
 type Tuple5TypeValidator = <A, B, C, D, E> (TypeValidator<A>, TypeValidator<B>, TypeValidator<C>, TypeValidator<D>, TypeValidator<E>) => TypeValidator<[A, B, C, D, E]>
 
 export const tuple5: Tuple5TypeValidator = function (va, vb, vc, vd, ve) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}]`
+  const tuple_value = () => [va.value(), vb.value(), vc.value(), vd.value(), ve.value()];
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 5) {
       return [va(value[0], _scope), vb(value[1], _scope), vc(value[2], _scope), vd(value[3], _scope), ve(value[4], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}]`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}]`
-  tuple.value = () => [va.value(), vb.value(), vc.value(), vd.value(), ve.value()];
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
   return tuple
 }
 
 type Tuple6TypeValidator = <A, B, C, D, E, F> (TypeValidator<A>, TypeValidator<B>, TypeValidator<C>, TypeValidator<D>, TypeValidator<E>, TypeValidator<F>) => TypeValidator<[A, B, C, D, E, F]>
 
 export const tuple6: Tuple6TypeValidator = function (va, vb, vc, vd, ve, vf) {
-  function tuple (value: mixed, _scope: string = '') {
+  const tuple_type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}, ${getType(vf)}]`
+  const tuple_value = () => [va.value(), vb.value(), vc.value(), vd.value(), ve.value(), vf.value()];
+  function tuple (value: mixed, _scope: string = '', err?: TypeAssertError[], _ctx: AssertionContext = {}) {
     if (Array.isArray(value) && value.length === 6) {
       return [va(value[0], _scope), vb(value[1], _scope), vc(value[2], _scope), vd(value[3], _scope), ve(value[4], _scope), vf(value[5], _scope)]
     }
-    throw validatorTypeError('tuple', `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}, ${getType(vf)}]`, value, _scope)
+    _ctx.assertion = false;
+    assertContext("tuple", tuple_type(), value, _scope, err, _ctx);
+    return tuple_value();
   }
-  tuple.type = () => `[${getType(va)}, ${getType(vb)}, ${getType(vc)}, ${getType(vd)}, ${getType(ve)}, ${getType(vf)}]`
-  tuple.value = () => [va.value(), vb.value(), vc.value(), vd.value(), ve.value(), vf.value()];
+  tuple.type = tuple_type;
+  tuple.value = tuple_value;
   return tuple
 }
