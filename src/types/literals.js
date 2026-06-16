dc42fe94b2173f17b3ea5868cd121f9870e3a8b2
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-06-16 12:26:53 +0200

    decouple literal() and literalOf()

--- src/types/literals.js
+++ src/types/literals.js
@@ -7,9 +7,21 @@ import { versions } from "../versions.js";
 import type { LiteralValue, TypeValidator, TypeAssertError, AssertionContext } from "..";
 
 export const literalOf =
-  <T: LiteralValue>(primitive: T): TypeValidator<T> => {
+  <T: LiteralValue>(primitive: T, label?: string = "", convert?: boolean = false): TypeValidator<T> => {
     deprwarn("calling literalOf is deprecated; fallback to literal; replace with literal validator instead; $Literal casting is not required", "FT001");
-    return literal(primitive);
+    function literal (value: mixed, _scope: string = label, err: ?TypeAssertError[], _ctx: AssertionContext = {}, _convert: boolean = convert): T {
+      _ctx.assertion = value === primitive;
+      assertContext(literal.name, literal.type(), value, _scope, err, _ctx.assertion);
+      return primitive;
+    }
+    literal.type = () => {
+      if (isUndef(primitive)) {
+        return "void";
+      }
+      return JSON.stringify(primitive) || "";
+    }
+    literal.value = (): T => primitive;
+    return literal;
   }
 
 export const literal =
