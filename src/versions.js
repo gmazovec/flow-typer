b2bccd31c1a7c5f88e2efe9bf647ee159862a443
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-06-18 13:07:08 +0200

    check for resolve
    the resolve() function is not supported on all versions of JS runtimes.

--- src/versions.js
+++ src/versions.js
@@ -2,10 +2,12 @@ import fs from "node:fs";
 
 export const versions = { flow: "" };
 
-const pkgUri = [import.meta.resolve('flow-bin/package.json')];
-if (pkgUri[0]) {
-	const pkgPath = decodeURI(pkgUri[0]).substr(7);
-	const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
-	versions.flow = pkg.version;
+if (import.meta.resolve) {
+  const pkgUri = [import.meta.resolve('flow-bin/package.json')];
+  if (pkgUri[0]) {
+    const pkgPath = decodeURI(pkgUri[0]).substr(7);
+    const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
+    versions.flow = pkg.version;
+  }
 }
 
