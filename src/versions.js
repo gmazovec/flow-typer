d141ece2461edc7304bb049e2435c7f48352907a
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-06-19 12:33:54 +0200

    unwrap import resolve promises
    the resolve() function might return promise and this commit handles
    both cases.

--- src/versions.js
+++ src/versions.js
@@ -5,6 +5,7 @@ export const versions = { flow: "" };
 if (import.meta.resolve) {
   const pkgUri = [import.meta.resolve('flow-bin/package.json')];
   if (pkgUri[0]) {
+    pkgUri[0] = await Promise.resolve(pkgUri[0]);
     const pkgPath = decodeURI(pkgUri[0]).substr(7);
     const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
     versions.flow = pkg.version;
