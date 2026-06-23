c6ff9fcec07361f2b0394df599600d6f3788c144
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-06-23 12:16:57 +0200

    update flow version requirement for literal()

--- src/types/literals.js
+++ src/types/literals.js
@@ -26,8 +26,8 @@ export const literalOf =
 
 export const literal =
   <const T> (primitive: T, label?: string = "", convert?: boolean = false): TypeValidator<T> => {
-    if (versions.flow && versions.flow < "0.269.0") {
-      console.error("literal() type checking is not supported; upgrade to version >= 0.269.0");
+    if (versions.flow && versions.flow < "0.269.1") {
+      console.error("literal() type checking is not supported; upgrade flow to version >= 0.269.1");
     }
     function literal (value: mixed, _scope: string = label, err: ?TypeAssertError[], _ctx: AssertionContext = {}, _convert: boolean = convert): T {
       _ctx.assertion = value === primitive;
