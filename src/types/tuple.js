ccef3af573989882d5c803ba3f91fa923b7f701a
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-07-01 20:49:56 +0200

    add tupleOf4.string() validator

--- src/types/tuple.js
+++ src/types/tuple.js
@@ -116,6 +116,10 @@ tuple4.number = function (value: mixed, _scope: string = "", err: ?TypeAssertErr
   return tuple4(number, number, number, number)(value, _scope, err, _ctx, convert);
 };
 
+tuple4.string = function (value: mixed, _scope: string = "", err: ?TypeAssertError[], _ctx: AssertionContext, convert?: boolean = false): [string, string, string, string] {
+  return tuple4(string, string, string, string)(value, _scope, err, _ctx, convert);
+};
+
 type Tuple5TypeValidator = <A, B, C, D, E> (TypeValidator<A>, TypeValidator<B>, TypeValidator<C>, TypeValidator<D>, TypeValidator<E>, label?: string, convert?: boolean) => TypeValidator<[A, B, C, D, E]>;
 
 export const tuple5: Tuple5TypeValidator = function (va, vb, vc, vd, ve, label = "", convert = false) {
