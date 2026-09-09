9c7e2e33ce35bfb3974d8e5e5b759733e2c3a52a
Gregor Mazovec <gregor.mazovec@gmail.com>
2026-09-09 13:20:13 +0200

    add map(T) validator
    map() validator maps only generic <T> values. The keys are always
    of a string type. The type is represented as plain object.

--- src/types/map.js
+++ src/types/map.js
@@ -2,7 +2,7 @@
 import { assertContext } from "../type.js";
 import { getType } from "../utils.js";
 import { validatorError } from "../error.js";
-import { boolean, number, string, undef } from "./primitives.js";
+import { boolean, number, string } from "./primitives.js";
 import { object } from "./object.js";
 import { unionOf } from "./union.js";
 import { deprwarn } from "../error.js";
@@ -46,3 +46,38 @@ mapOf.boolean = (createMapOfValidator(boolean): TypeMapValidator<boolean>);
 mapOf.number = (createMapOfValidator(number): TypeMapValidator<number>);
 
 mapOf.string = (createMapOfValidator(string): TypeMapValidator<string>);
+
+export const map = <V>
+  (
+    typeFn: TypeValidator<V>,
+    label?: string = "Object",
+    convert?: boolean = false
+  ): TypeMapValidator<V> => {
+    const type = () => `{ [string]: ${getType(typeFn)} }`;
+    function map (value: mixed, _scope: string = label, err: ?TypeAssertError[], _ctx: AssertionContext = {}, _convert: boolean = convert) {
+      const o = object(value, _scope, err, _ctx, _convert);
+      assertContext("map", type(), value, _scope, err, _ctx.assertion);
+      const reducer = (acc: { [string]: V }, key: string) =>
+        Object.assign(
+          acc,
+          {
+            [key]: typeFn(o[key], `${_scope}.${key}`, err, _ctx, _convert)
+          }
+        );
+      return Object.keys(o).reduce(reducer, {});
+    }
+    map.type = type;
+    map.value = (): { [string]: V } => ({});
+    return map;
+  };
+
+function createMapValidator <T> (typeFn: TypeValidator<T>, label?: string = "Object", convert?: boolean = false): TypeValidator<{ [string]: T }> {
+  return map(typeFn, label, convert);
+}
+
+map.boolean = (createMapValidator(boolean): TypeMapValidator<boolean>);
+
+map.number = (createMapValidator(number): TypeMapValidator<number>);
+
+map.string = (createMapValidator(string): TypeMapValidator<string>);
+
